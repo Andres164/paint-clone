d6e990f58fddfdbc692aab24a7fbfbdab39b246b
Andres Castro <andres435b@gmail.com>
2022-10-27 15:02:03 -0600

    Started working on the code arrangement

---
+++ GraficacionAndresCastro/GraficacionAndresCastro/Classes/Brush.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraficacionAndresCastro.Classes
+{
+    internal class Brush
+    {
+        enum Sizes { Small, Medium, Big }
+
+        Color selectedColor;
+        Sizes selectedSize;
+        Bitmap canvas;
+
+        public Brush(Color )
+        {
+
+        }
+        public void drawPixelOnBitmap(Point coordinates)
+        {
+
+        }
+    }
+}
